bee208fcd55358e7256af594bed2f68906d35af5
wd <2658357859@qq.com>
2020-03-22 20:51:44 +0800

    改了一些东西

---
+++ src/app.js
@@ -0,0 +1,14 @@
+import Vue from 'vue'
+import Buttons from './components/button.vue'
+import './style/index.css'
+
+new Vue({
+    data() {
+        return {
+
+        }
+    },
+    components: {
+        Buttons
+    }
+}).$mount("#app")
---
+++ src/components/button.vue
@@ -0,0 +1,30 @@
+<template>
+    <button class="x-button"> 按钮 </button>
+</template>
+<script>
+    export default {
+        name: 'Buttons',
+        data() {
+            return {
+
+            }
+        },
+        methods: {
+
+        }
+    }
+</script>
+<style lang="scss">
+    .x-button {
+        font-size: var(--font-size);
+        height: var(--button-height);
+        padding: 0 1em;
+        border-radius: var(--border-radius);
+        border: 1px solid var(--border-color);
+        background: var(--button-bg);
+        &:hover {
+            background-color: var(--border-color);
+        }
+    }
+
+</style>
